04eedac8082e2889711a32e7046ce7b84a3dcd19
Luis Aseguinolaza <luisasg_2001@hotmail.com>
2019-11-12 16:17:12 -0300

    Franquicia estaba implementadno y tenia que extender

--- src/Tarjeta_Franquicia_Completa.php
+++ src/Tarjeta_Franquicia_Completa.php
@@ -2,7 +2,7 @@
 
 namespace TrabajoTarjeta;
 
-class Tarjeta_Franquicia_Completa implements TarjetaInterface {
+class Tarjeta_Franquicia_Completa extends Tarjeta {
     protected $valor = 0.0;
     protected $caso = 'Franquicia Completa';
 
@@ -12,7 +12,9 @@ class Tarjeta_Franquicia_Completa implements TarjetaInterface {
      * @return true 
      *      Como la franquicia completa siempre es capaz de pagar un viaje la funcion solo devuelve true
      */
-    public function pagar_tarjeta( ColectivoInterface $colectivo , $tiempo ) {
+    public function pagarConTarjeta( ColectivoInterface $colectivo , $tiempo ) {
+		$this->setUltimoPago( $tiempo );
+        $this->setUltimoColectivo( $colectivo );
         return new Boleto($colectivo, $this, $valor, $caso , $tiempo );;
     }
 	
--- tests/DatosBoletoTest.php
+++ tests/DatosBoletoTest.php
@@ -5,7 +5,7 @@ namespace TrabajoTarjeta;
 use PHPUnit\Framework\TestCase;
 
 class Datos_Boleto_Test extends TestCase {
-
+use Costos;
     /**
      * Comprueba que los datos de un boleto se asignan correctamente al pagar con una tarjeta en un colectivo
      * 
@@ -18,10 +18,10 @@ class Datos_Boleto_Test extends TestCase {
         $colectivo = new Colectivo( 'mixta', '103', 420 );
         $tarjeta->recargar( 20.0 );
         $boleto = $tarjeta->pagarConTarjeta( $colectivo , $tiempo_prueba );
-        $this->assertEquals( $boleto->obtener_valor(), 14.80 );
-        $this->assertEquals( $boleto->obtener_saldo(), 5.20 );
-        $this->assertEquals( $boleto->obtener_tarjeta_id(), $tarjeta->obtener_id() );
-        $this->assertEquals( $boleto->obtener_linea(), '103' );
+        $this->assertEquals( $boleto->tarjeta->getCostoUltimoViaje(), $this->getCostoViaje() );
+        $this->assertEquals( $boleto->tarjeta->getSaldo(), 20.0 - $this->getCostoViaje() );
+        $this->assertEquals( $boleto->tarjeta->getId(), NULL );
+        $this->assertEquals( $boleto->colectivo->linea(), '103' );
         $this->assertEquals( $boleto->obtener_tipo_boleto(), 'Normal' );
     }
 
