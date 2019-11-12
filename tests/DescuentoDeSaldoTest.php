e49a82afa3f71a02cbfae1e69c156f0903d6d092
Luis Aseguinolaza <luisasg_2001@hotmail.com>
2019-11-12 17:53:14 -0300

    Arreglando Failures de CircleCI

--- tests/DescuentoDeSaldoTest.php
+++ tests/DescuentoDeSaldoTest.php
@@ -16,7 +16,7 @@ class DescuentodesaldoTest extends TestCase {
         $tarjeta->recargar( 100.0 );
         $tarjeta->gastarPlus();
 		$boleto = $tarjeta->pagarConTarjeta( $colectivo , $tiempo_prueba );
-        $this->assertEquals( $boleto->getValor() , ( 100.0 -  ( 2 * $this->getCostoViaje() ) ) );
+        $this->assertEquals( $tarjeta->getSaldo() , ( 100.0 -  ( 2 * $this->getCostoViaje() ) ) );
     }
 
     /**
@@ -26,12 +26,12 @@ class DescuentodesaldoTest extends TestCase {
      */
     public function test_descuento_de_dos_plus() {
         $tiempo_prueba = new Tiempo();
-        $tarjeta = new Tarjeta(  NULL );
+        $tarjeta = new Tarjeta( NULL );
         $colectivo = new Colectivo( 'mixta', '103', 420 );
         $tarjeta->recargar( 100.0 );
         $tarjeta->gastarPlus();
         $tarjeta->gastarPlus();
 		$boleto = $tarjeta->pagarConTarjeta( $colectivo , $tiempo_prueba );
-        $this->assertEquals( $boleto->getValor(), ( 100.0 -  ( 3 * $this->getCostoViaje() ) ) );
+        $this->assertEquals( $tarjeta->getSaldo(), ( 100.0 -  ( 3 * $this->getCostoViaje() ) ) );
     }
 }
